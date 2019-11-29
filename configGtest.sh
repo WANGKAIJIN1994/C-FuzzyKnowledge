d7b6da7a097664fa059bed100380a3ea3a5b820a
root <root@localhost.localdomain>
2019-11-30 01:15:38 +0800

    config gtest env

---
+++ configGtest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# create include & lib
+mkdir include
+mkdir lib
+
+# get gtest 
+git clone git@github.com:google/googletest.git
+
+# cp gtest include
+cp googletest/googletest/include/gtest/ include/ -rf
+
+# create buildout file
+cd googletest/googletest/
+mkdir buildout
+cd buildout
+
+# build static library
+rm * -rf
+cmake ../
+make 
+cp lib/libgtest* ../../../lib/ -rf
+
+# build dynamic library
+rm * -rf
+cmake -DBUILD_SHARED_LIBS=ON ../
+make
+cp lib/libgtest* ../../../lib/ -rf
+
+# clear 
+cd ../../../
+rm googletest -rf
+
