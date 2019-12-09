cf94f6194226e8366b14ac06ecadb98186fe11e5
Kai Wang C <kai.c.wang@ericsson.com>
2019-12-09 13:15:30 +0100

    strtok_r
    Change-Id: I3e2279a1eecc53e4554b5127a30bf134dcb255a4

--- src/string.cpp
+++ src/string.cpp
@@ -6,7 +6,7 @@ std::string::size_type onlyEnds_with(const std::string& str, const std::string& 
 {
     std::string::size_type n = str.find(subStr);
 
-    if (n != std::string::npos && str.compare(n, str.size() - n, subStr) == 0)
+    if (n != std::string::npos && str.substr(n).size() == subStr.size())
     {
         return n;
     }
@@ -35,4 +35,47 @@ TEST(String, char2String)
 
     std::string str1(chr, 8);
     EXPECT_EQ(str1.size(), 8);
+}
+
+TEST(String, std_copy_n)
+{
+    char* chr = new char[9];
+    char chr1[] = { '2', '3', '4', '5', '6', '7', '8', '9', '\0'};
+    std::cout << chr1 << std::endl;
+
+    std::copy_n(chr1, 9, chr);
+    std::cout << chr << std::endl;
+
+    delete []chr;
+}
+
+TEST(String, std_copy_n_INT)
+{
+    long interfaceId = 345;
+    unsigned char addr[8];
+
+    std::copy_n((char*)&interfaceId, sizeof(long), addr);
+    std::cout << *(long*)addr << std::endl;
+}
+
+TEST(String, substr)
+{
+    std::string str("123456789");//9
+
+    std::string::size_type n = str.find("789");//6
+    std::cout << n << std::endl;
+
+    std::string substr = str.substr(n + 3);
+    std::cout << substr << std::endl;
+
+}
+
+TEST(String, strtok_r)
+{
+    char str[] = "    123 456";
+
+    char *tmp;
+    char *ptr = strtok_r(str, " ", &tmp);
+
+    std::cout << ptr << std::endl;
 }
